bac6f50387000a5e16f8c94a4b2a64334ee6606a
embermind <embermind@naver.com>
2023-02-27 20:54:41 +0900

    Practice Image View

--- PictureBox/ViewController.swift
+++ PictureBox/ViewController.swift
@@ -8,12 +8,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var imgIndex:Int = 1
+    var viewImg: UIImage?
+    @IBOutlet var imgView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        viewImg = UIImage(named: "1.jpeg")
+        imgView.image = viewImg
     }
-
-
+    @IBAction func btnPrevious(_ sender: UIButton) {
+        imgIndex = imgIndex - 1
+        if(imgIndex < 1){
+            imgIndex = 1
+        }
+        let imgName = String(imgIndex) + ".jpeg"
+        viewImg = UIImage(named: imgName)
+        imgView.image = viewImg
+    }
+    @IBAction func btnNext(_ sender: UIButton) {
+        imgIndex = imgIndex + 1
+        if(imgIndex > 2){
+            imgIndex = 2
+        }
+        let imgName = String(imgIndex) + ".jpeg"
+        viewImg = UIImage(named: imgName)
+        imgView.image = viewImg
+    }
+    
 }
 
